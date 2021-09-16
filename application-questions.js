024e8b1edb23aa41fdb7b7b0546b2925c12c9bbd
Aking16 <81476541+Aking16@users.noreply.github.com>
2021-09-16 20:09:26 +0430

    Add files via upload

---
+++ application-questions.js
@@ -0,0 +1,12 @@
+module.exports =  [
+	"Your application shouldn't be more than **2000** words.\nROBLOX Username:",
+	"Discord Username: [Username#1111]",
+	"USM Rank:",
+	"Are you in any divisions?",
+	"1. What position are you applying for?",
+	"2. Why should we hire you?",
+	"3. What will you bring us as the position you are applying?",
+	"4. Any past experiences? (Specify them if so)",
+	"5. Rate your activity. [1-10]",
+	"6. Rate your maturity. [1-10]"
+];
